aca22c98c87f941cb5a2587ce383007aa8e773ef
ZK <zhang2121619kai@gmail.com>
2015-08-30 21:51:38 +0800

    Detecting the order of receiving

--- 3nodes_msg_passing.c
+++ 3nodes_msg_passing.c
@@ -40,18 +40,18 @@ int main(int argc, char *argv[]){
 		MPI_Recv (msg_1, 20, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
 		print (my_rank, "finished receiving msg");
 
-		MPI_Send (msg_1, strlen(msg_1)+1, MPI_CHAR, 3, my_rank, MPI_COMM_WORLD);
+		MPI_Send (msg_1, strlen(msg_1)+1, MPI_CHAR, 2, my_rank, MPI_COMM_WORLD);
 		print (my_rank, "finished sending msg");
 	}
 	else if (my_rank == 2){
 		MPI_Recv (msg_1, 20, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		if (msg_1 == "Hello")
+		if (msg_1[0] == 'H')
 			print (my_rank, "finished receiving the first message");
 		else 
 			print (my_rank, "finished receiving the second message");
 
 		MPI_Recv (msg_2, 20, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		if (msg_2 == "Hello")
+		if (msg_2[0] == 'H')
 			print (my_rank, "finished receiving the first message");
 		else 
 			print (my_rank, "finished receiving the second message");
